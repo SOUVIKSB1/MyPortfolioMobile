e9c112ec87bd66226167c9382074cb244ca2095b
Souvik Sinhababu <user.email like "souviksinhababu1@gmail.com>
2026-05-29 17:33:16 +0530

    fix server

--- backend/server.js
+++ backend/server.js
@@ -125,7 +125,7 @@ app.post('/api/reviews', async (req, res) => {
 // Serve frontend build in production (relative path from backend folder to root dist)
 app.use(express.static(path.join(__dirname, '../dist')));
 
-app.get('*', (req, res) => {
+app.get('/{*splat}', (req, res) => {
   res.sendFile(path.join(__dirname, '../dist', 'index.html'));
 });
 
