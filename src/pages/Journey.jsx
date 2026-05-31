df34a514846257a399ffcfbcf5f5243621835501
Souvik Sinhababu <user.email like "souviksinhababu1@gmail.com>
2026-05-31 22:53:35 +0530

    Journey

--- src/pages/Journey.jsx
+++ src/pages/Journey.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { motion, AnimatePresence } from "framer-motion";
-import { Award, Briefcase, GraduationCap, Calendar, Tag, School, BookOpen } from "lucide-react";
+import { Award, Briefcase, GraduationCap, Calendar, Tag, School, BookOpen, BookOpenCheck } from "lucide-react";
 import "./Journey.css";
 import { FaSchoolCircleExclamation } from "react-icons/fa6";
 
@@ -32,7 +32,7 @@ const TIMELINE = [
   {
     date: "2018",
     type: "Education",
-    icon: <School size={16} />,
+    icon: <BookOpenCheck size={16} />,
     title: "Secondary Education (Madhyamik)",
     institution: "Kenduadihi High School - Bankura, West Bengal",
     description: "Achieved 645 out of 700 marks (92.14%)."
